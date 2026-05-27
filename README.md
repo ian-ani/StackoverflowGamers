7dd82559dc038aca43dcfc16b5836a5dc9b6d86f
Iris <irisaunav@gmail.com>
2026-05-27 14:19:26 +0200

    Add backend

--- README.md
+++
@@ -1,2 +0,0 @@
-# StackoverflowGamers
-Reto final.
--- src/main/java/com/stackoverflowgamers/backend/repository/TypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackoverflowgamers.backend.repository;
-
-import com.stackoverflowgamers.backend.entity.Type;
-import lombok.NonNull;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRepository extends JpaRepository<@NonNull Type, @NonNull Integer> { }
